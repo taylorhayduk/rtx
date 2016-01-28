446b752fbf626ac2f53a32f17652f71fa020057a
Taylor Hayduk <taylorhayduk@gmail.com>
2016-01-27 19:11:08 -0500

    (fix) Added process.env.PORT for Heroku deployment

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.send('blah');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
 });
